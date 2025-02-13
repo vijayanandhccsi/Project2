03bcb2bd7110c2adffa8232a88fdd4bf2689ba00
vijayanandhccsi <56343142+vijayanandhccsi@users.noreply.github.com>
2025-02-13 15:17:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1" # Change this to your desired AWS region
-}
-
 # Create a VPC
 resource "aws_vpc" "main_vpc" {
   cidr_block           = "10.0.0.0/16"
