757c1360fc98d2ad9544c776c751905a56337c64
vijayanandhccsi <56343142+vijayanandhccsi@users.noreply.github.com>
2025-02-13 15:26:57 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 3.0"
+      source = "hashicorp/aws"
+      version = "5.86.1"
     }
   }
 }
 
-provider "aws" {
+Provider "aws" {
   region = "us-east-1"
 }
